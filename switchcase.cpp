7bb6d027ef98ef7c4b3f773c343bd8c7726465a2
harshsj1504 <harshjoshi1504@outlook.com>
2024-12-26 19:49:21 +0530

    added switchcase

---
+++ switchcase.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n=1;
+    switch (n){
+        case 1:
+        cout<<"first"<<endl;
+       
+        case 2:
+        cout<<"second"<<endl;
+        break;
+        default :
+        cout<<"default"<<endl;
+    }
+}
