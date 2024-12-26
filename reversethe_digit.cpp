04dd3f70c600ea041bf4117762087dfb13cb08d9
harshsj1504 <harshjoshi1504@outlook.com>
2024-12-26 19:23:35 +0530

    updated solution

--- reversethe_digit.cpp
+++ reversethe_digit.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-class Solution {
-public:
-    int reverse(int x) {
+
+int reverse(int x) {
         int ans=0;
         while (x!=0){
             int digit = x%10;
-            if ((ans > INT_MAX/10)||(ans < INT_MIN/10)){
+            if ((ans > INT32_MAX/10)||(ans < INT32_MIN/10)){
                 return 0;
             }
             ans = (ans*10)+digit;
@@ -13,5 +12,4 @@ public:
 
         }
         return ans;
-    }
-};
+}
