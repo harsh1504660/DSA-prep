9cc96442b505b3788fa317a8be713615ae9adbb4
harshsj1504 <harshjoshi1504@outlook.com>
2025-01-03 20:29:57 +0530

    added aggressive cows

--- day1.cpp
+++ day1.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 int main(){
     cout<<"hello world fuck you ";
     int a = 123;
     cout<<a<<endl;
     char d = 'd';
-    cout<<"i am harsh and this is : "<<d<<endl;
+    cout<<"i am harsh and this is : \a"<<d<<endl;
     int size = sizeof(a);
     cout<<size<<endl;
     return 0;
