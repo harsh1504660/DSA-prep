47082edd3dc21cd4e284ea9b8256d770e4ea58ff
harsh1504660 <harshsmj1504@gmail.com>
2025-02-10 03:03:26 +0530

    added dll

--- LinkedList/DoublyLinkedList.cpp
+++ LinkedList/DoublyLinkedList.cpp
@@ -18,14 +18,35 @@ class Node {
 void print(Node* head){
     Node* temp = head;
     while(temp!=NULL){
-        cout<<temp->data;
+        cout<<temp->data<<" ";
         temp = temp->next;
     }
     cout<<endl;
 }
-
+int getLength(Node* head){
+    int len =0;
+    Node* temp = head;
+    while(temp!=NULL){
+        len++;
+        //cout<<temp->data;
+        temp = temp->next;
+    }
+    return len;
+}
+void InsertAthead(Node* & head,int data){
+    Node* temp = new Node(data);
+    temp ->next = head;
+    head-> prev = temp;
+    head = temp;
+}
 int main()
 {
     Node* node1 = new Node(10);
+    Node* head = node1 ;
+    print(head);
+    cout<<getLength(head)<<endl;
+    InsertAthead(head,11);
+    print(head);
     return 0;
 }
+
