fdbcd3e959714c9b027f51c0ebdabc0cac597301
harsh1504660 <harshsmj1504@gmail.com>
2025-02-10 23:35:30 +0530

    added Reverse the linked list problem

---
+++ LinkedList/ReversetheLinkedList/Approach1_Iterative.cpp.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    public :
+    int data;
+    Node *next;
+    Node(int data){
+        this-> data = data;
+        this-> next = NULL;
+
+    }
+};
+
+Node* reverseLinkedList(Node* head){
+    if (head == NULL || head -> next ==NULL){
+        return head;
+    }
+    Node* prev = NULL;
+    Node* forward = NULL;
+    Node* curr = head;
+    while(curr != NULL){
+        forward = curr ->next;
+        curr -> next = prev;
+        prev = curr;
+        curr = forward ;
+    }
+    return prev;
+
+}
+
+int main()
+{
+    
+    return 0;
+}
