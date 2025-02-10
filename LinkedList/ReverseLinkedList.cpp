ad139d8805a056f893cc14e3157c5f145120e1cd
harsh1504660 <harshsmj1504@gmail.com>
2025-02-10 23:20:06 +0530

    added revertse the linked list problem

---
+++ LinkedList/ReverseLinkedList.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    public :
+    int data;
+    Node *next;
+    Node(int data){
+        this-> data = data;
+        this-> next = NULL;
+
+    }
+};
+
+Node* reverseLinkedList(Node* head){
+    
+}
+
+int main()
+{
+    
+    return 0;
+}
