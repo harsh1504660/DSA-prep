50674b61ade9deb03a1ee522bb339c092ddb0943
harsh1504660 <harshsmj1504@gmail.com>
2025-02-12 02:08:33 +0530

    added cycle detection problem

---
+++ LinkedList/DetectLoopinList.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class Node {
+    public :
+    int data;
+    Node* next;
+
+    //constructor
+    Node(int data){
+        this -> data = data;
+        this ->next = NULL;
+
+    }
+    ~Node (){
+        int value = this->data;
+        if(this->next !=NULL){
+            delete next;
+            next =NULL;
+
+        }
+        cout<<"Memroy is deleted for value : "<<value<<endl;
+    }
+};
+int main()
+{
+    
+    return 0;
+}
