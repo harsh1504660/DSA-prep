1659acce44f131dd14ce5b17dd35ce559cd5da81
harsh1504660 <harshsmj1504@gmail.com>
2025-02-11 18:33:34 +0530

    added iscurcularlist problem solution

--- LinkedList/ReversetheLinkedList/ReverseInKGroup.cpp
+++ LinkedList/ReversetheLinkedList/ReverseInKGroup.cpp
@@ -11,6 +11,34 @@ class Node {
 
     }
 };
+
+Node* kReverse(Node* head , int k){
+    //base case 
+    if(head==NULL){
+        return NULL;
+    }
+    //STEP1 : REVERSE K NODES
+    Node* next = NULL;
+    Node* curr = head;
+    Node* prev = NULL;
+    int count = 0 ; 
+
+    while(curr != NULL && count < k){
+        next = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = next;
+        count++;
+    }
+
+    //step 2  recurstion dekhega aageka
+    if(next != NULL){
+        head -> next = kReverse(next,k);
+    }
+
+    //step 3 return head of reverse list 
+    return prev;
+}
 int main()
 {
     
