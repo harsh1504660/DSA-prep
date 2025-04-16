cf0b071fee999fda374b29b47ca74e1ba991584e
harsh1504660 <harshsmj1504@gmail.com>
2025-04-16 17:22:14 +0530

    added depth of tree problem

---
+++ BinaryTree/DepthofTree.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+int height(node* root){
+
+    //base case
+    if(root==NULL){
+        return 0;
+    }
+
+    int left = height(root->left);
+    int right = height(root->right);
+
+    int ans = max(left,right) +1;
+
+    return ans;
+
+}
+int main()
+{
+    
+    return 0;
+}
