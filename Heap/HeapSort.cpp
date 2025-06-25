985e867e97a37dce84f066c7fca4cafa0a788106
harsh1504660 <harshsmj1504@gmail.com>
2025-06-25 19:31:17 +0530

    added heap sort algo

---
+++ Heap/HeapSort.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+//ologn
+class heap{
+    public:
+    int arr[100];
+    int size = 0 ; 
+
+    heap(){
+        arr[0] = -1;
+        size=0;
+    }
+    void insert(int value){
+        size = size+1;
+        int index= size; 
+        arr[index] = value;
+
+
+        while(index>1){
+            int parent = index/2;
+
+            if(arr[parent]<arr[index]){
+                swap(arr[parent],arr[index]);
+            }
+            else{
+                return;
+            }
+        }
+    }
+    void print(){
+        for(int i = 1 ; i<=size;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+};
+
+void heapify(int arr[],int n , int i){
+    int largest = i ; 
+    int left = 2*i;
+    int right = 2*i+1;
+
+    if(left<n && arr[largest]<arr[left]){
+        largest = left;
+    }
+        if(right<n && arr[largest]<arr[right]){
+        largest = right;
+    }
+
+    if(largest !=i){
+        swap(arr[largest],arr[i]);
+        heapify(arr,n,largest);
+    }
+}
+
+
+void heapSort(int arr[],int n){
+    int size= n ;
+    while(size>1){
+        //step 1 : swap
+        swap(arr[size],arr[1]);
+        
+        //step 2
+        size--;
+
+        //step 3
+        heapify(arr,size,1);
+    }
+}
+
+int main()
+{
+    heap  h;
+    h.insert(50);
+    h.insert(55);
+    h.insert(53);
+    h.insert(52);
+    h.insert(54);
+    h.print();
+    return 0;
+}
