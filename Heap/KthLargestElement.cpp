bef027fed6b65413c6525a8e0b822cbacc9b85fd
harsh1504660 <harshsmj1504@gmail.com>
2025-06-26 00:14:02 +0530

    added problem kth smallest

---
+++ Heap/KthLargestElement.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<queue>
+using namespace std;
+
+
+int kthElemnet(int arr[],int l,int r,int k){
+    priority_queue<int> pq;
+    for(int i = 0;i<k;i++){
+        //step 1 : 
+        pq.push(arr[i]);
+    }
+
+    //step2 :
+    for(int i = k ; i<r ; i++){
+        if(arr[i] < pq.top()){
+            pq.pop();
+            pq.push(arr[i]);
+
+        }
+    }
+    // ans  = pq.top
+   int ans = pq.top();
+   return ans;
+   
+}
+
+int main()
+{
+    
+    return 0;
+}
